a5396946fc147c85dbd2bd3c7ecc4c1ecd8e0dee
laksh <lakshyam38@gmail.com>
2022-02-22 19:13:00 +0530

    contains dup python

---
+++ python/blind curated 75/contains_duplicate.py
@@ -0,0 +1,16 @@
+# Leetcode 217
+
+
+def containsDuplicate(nums: list) -> bool:
+    unique = set()
+
+    for i in nums:
+        if i in unique:
+            return True
+        unique.add(i)
+
+    return False
+
+
+print(containsDuplicate([1, 2, 3, 1]))  # False
+print(containsDuplicate([1, 2, 3]))  # True
